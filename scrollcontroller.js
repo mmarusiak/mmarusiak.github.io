35890f5fd9fa2f7d3e7ce0f26dd5916fe3fcd974
Marcel Marusiak <marcel.marusiak@gmail.com>
2023-02-18 16:58:48 +0100

    Update scrollcontroller.js

--- scrollcontroller.js
+++ scrollcontroller.js
@@ -1,17 +1,3 @@
-$(function() {
-    var controller = new ScrollMagic.Controller();
-    var blockTween = new TweenMax.to('#block', 1.5, {
-        backgroundColor: 'red'
-    });
-    
-    var containerScene = new ScrollMagic.Scene({
-        triggerElement: '#container'
-    })
-    .setTween(blockTween)
-    .addIndicators()
-    .addTo(controller);
-});
-
 function changeToPredictor(){
     window.location.href = "Projects/JS-Neural-Network/color_predictor_demo/color_demo.html";
 }

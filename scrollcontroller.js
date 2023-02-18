f9ede3833a39dcf9d9b662a7c42bc7e94f2975b1
Marcel Marusiak <marcel.marusiak@gmail.com>
2023-02-18 16:56:34 +0100

    Added: Buttons to navigate

--- scrollcontroller.js
+++ scrollcontroller.js
@@ -11,3 +11,11 @@ $(function() {
     .addIndicators()
     .addTo(controller);
 });
+
+function changeToPredictor(){
+    window.location.href = "Projects/JS-Neural-Network/color_predictor_demo/color_demo.html";
+}
+
+function changeToXOR(){
+    window.location.href = "Projects/JS-Neural-Network/xor_demo/xor_demo.html";
+}

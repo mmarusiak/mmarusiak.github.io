de5193abf93cd472b5b605d73ee83594e5a4e4ca
Marcel Marusiak <marcel.marusiak@gmail.com>
2023-08-08 20:59:01 +0200

    Added links panel

--- actions.js
+++
@@ -1,7 +0,0 @@
-function changeToPredictor(){
-    window.location.href = "Projects/JS-Neural-Network/color_predictor_demo/color_demo.html";
-}
-
-function changeToXOR(){
-    window.location.href = "Projects/JS-Neural-Network/xor_demo/xor_demo.html";
-}

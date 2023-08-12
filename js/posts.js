3f220a83325e9365ea6475c8e153ec8138b3bebc
Marcel Marusiak <marcel.marusiak@gmail.com>
2023-08-12 20:22:11 +0200

    WIP: Hashtags to posts

--- js/posts.js
+++ js/posts.js
@@ -1,30 +1,42 @@
-async function getTextFromElementsWithId(ids) {
+async function getPostsInfo(ids = ['post-title', 'post-summary'], hashtagClass = 'hashtag') {
     const response = await fetch('./Posts/posts.json');
     const fileList = await response.json();
 
-    const textContents = [];
+    const posts = [];
 
     for (const fileName of fileList) {
         const fileResponse = await fetch(fileName);
         const fileText = await fileResponse.text();
-        
+
 
         const parser = new DOMParser();
         const doc = parser.parseFromString(fileText, 'text/html');
-        post = {title : "title", content : "content", source : "source"}
-        for (const id of ids){
+        const post = { title: "title", content: "content", source: "source", hashtags: [] }
+        for (const id of ids) {
             const element = doc.getElementById(id);
             if (element) {
-                if(ids.indexOf(id) == 0){
+                if (ids.indexOf(id) == 0) {
                     post.title = element.textContent;
-                }else{
+                } else {
                     post.content = element.textContent;
                 }
             }
         }
+        const hashElements = doc.getElementsByClassName(hashtagClass);
+        for (const hashElement of hashElements) {
+            post.hashtags.push(hashElement.textContent);
+        }
         post.source = fileName;
-        textContents.push(post);
+        posts.push(post);
     }
+    return posts;
+}
 
-    return textContents;
+function loadHashtags(divHashtag, hashtags) {
+    console.log(hashtags)
+    for (let i = 0; i < hashtags.length; i++) {
+        const newP = document.createElement("p");
+        newP.textContent = hashtags[i];
+        divHashtag.appendChild(newP);
+    }
 }

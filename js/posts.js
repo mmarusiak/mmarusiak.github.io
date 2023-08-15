6c77b0cfdfb9f8cb74f8cf685954981ad24f6956
Marcel Marusiak <marcel.marusiak@gmail.com>
2023-08-15 12:40:50 +0200

    WIP: Posts - added dates to posts

--- js/posts.js
+++ js/posts.js
@@ -1,4 +1,19 @@
-async function getPostsInfo(ids = ['post-title', 'post-summary'], hashtagClass = 'hashtag') {
+const months = [
+    'Jan.',
+    'Feb.',
+    'Mar.',
+    'Apr.',
+    'May.',
+    'Jun.',
+    'Jul.',
+    'Aug.',
+    'Sep.',
+    'Oct.',
+    'Nov.',
+    'Dec.'
+]
+
+async function getPostsInfo(ids = ['post-title', 'post-summary', 'date'], hashtagClass = 'hashtag') {
     const response = await fetch('./Posts/posts.json');
     const fileList = await response.json();
 
@@ -11,14 +26,24 @@ async function getPostsInfo(ids = ['post-title', 'post-summary'], hashtagClass =
 
         const parser = new DOMParser();
         const doc = parser.parseFromString(fileText, 'text/html');
-        const post = { title: "title", content: "content", source: "source", hashtags: [] }
+        const post = 
+        {
+             title: "title", 
+             content: "content", 
+             date: "post-date", 
+             dateConverted: "date-converted", 
+             source: "source", 
+             hashtags: [] 
+            }
         for (const id of ids) {
             const element = doc.getElementById(id);
             if (element) {
                 if (ids.indexOf(id) == 0) {
                     post.title = element.textContent;
-                } else {
+                } else if (ids.indexOf(id) == 1){
                     post.content = element.textContent;
+                }else {
+                    post.date = element.textContent;
                 }
             }
         }
@@ -27,9 +52,26 @@ async function getPostsInfo(ids = ['post-title', 'post-summary'], hashtagClass =
             post.hashtags.push(hashElement.textContent);
         }
         post.source = fileName;
+        post.dateConverted = getDateFromRaw(post.date);
+        console.log(post)
         posts.push(post);
     }
     return posts;
+}
+
+function getDateFromRaw(rawDate){
+    const dateSliced = rawDate.split(' ');
+    const month = months.indexOf(dateSliced[0]) + 1;
+    let monthStr = month.toString();
+    if(month < 10){
+        monthStr = '0' + monthStr;
+    }
+    const day = dateSliced[1];
+    const year = dateSliced[2];
+
+    const fullDate = year + '-' + monthStr + '-' + day;
+    const date = new Date(fullDate);
+    return(date);
 }
 
 function loadHashtags(divHashtag, hashtags) {

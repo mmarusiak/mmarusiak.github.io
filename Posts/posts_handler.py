ff5da03d78c71fbf519a11b95af5141cdd5af996
Marcel Marusiak <marcel.marusiak@gmail.com>
2023-08-25 18:37:09 +0200

    Update posts_handler.py

--- Posts/posts_handler.py
+++ Posts/posts_handler.py
@@ -1,13 +1,12 @@
 import os
 import json
 
-# Replace 'path/to/your/directory' with the actual path of your directory
 directory_path = './Posts/'
 file_list = []
 
 for root, _, files in os.walk(directory_path):
     for file in files:
-        if(file.__contains__('.html')):
+        if(file.__contains__('.html') and !file.__contains__('template')):
             file_list.append(os.path.join(root, file))
 
 with open('./Posts/posts.json', 'w') as json_file:

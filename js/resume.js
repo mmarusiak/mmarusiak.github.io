6eab8e286a73c88af3b6e51ae36805deb20996ed
Marcel <marcel.marusiak@gmail.com>
2024-09-30 21:54:21 +0200

    Added navigation to resume page

--- js/resume.js
+++ js/resume.js
@@ -6,6 +6,8 @@ const pdfElements = document.getElementById('pdf');
 const navInt = document.getElementById('nav-int');
 const navRes = document.getElementById('nav-res');
 
+const navMain = document.getElementById('main-page');
+
 function changeDisplay(targetState){
     interactive = targetState;
     interactiveElements.className = "interactive-" + interactive;
@@ -22,4 +24,8 @@ navRes.addEventListener('click', function(){
     navInt.className = 'nav-resume-inter';
     navRes.className = 'nav-resume-pressed';
     changeDisplay(false);
+});
+
+navMain.addEventListener('click', function(){
+    window.location.href = './';
 });
